a5fac21b772b871728da0c92e00d2d7dab6cffba
meathirarose <athirarosejohn@gmail.com>
2025-01-21 15:05:06 +0530

    chore: update configurations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-//Re-export files from errors and middlewares
+//Re-export files from errors
 export * from './errors/BadRequestError';
 export * from './errors/CustomError';
 export * from './errors/DatabaseConnectionError';
@@ -6,6 +6,7 @@ export * from './errors/NotAuthorizedError';
 export * from './errors/NotFoundError';
 export * from './errors/RequestValidationError';
 
+//Re-export files from middlewares
 export * from './middlewares/currentUser';
 export * from './middlewares/errorHandler';
 export * from './middlewares/requireAuth';
