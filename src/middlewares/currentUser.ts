24344b37ed72a50e641f6c370eeea00105383db7
meathirarose <athirarosejohn@gmail.com>
2025-02-13 11:43:48 +0530

    chore: update configurations

--- src/middlewares/currentUser.ts
+++ src/middlewares/currentUser.ts
@@ -1,29 +1,33 @@
-// import { Request, Response, NextFunction } from 'express';
-// import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
+interface UserPayload {
+    id: string,
+    email: string
+}
 
-// declare global {
-//     namespace Express {
-//         interface Request {
-//             currentUser?: UserPayload;
-//         }
-//     }
-// }
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload;
+        }
+    }
+}
 
-// export const currentUser = (req: Request, res: Response, next: NextFunction) => {
-//     const token = req.cookies.accessToken;
+export const currentUser = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.accessToken;
 
-//     if(!token){
-//         return next();
-//     }
+    if(!token){
+        return next();
+    }
 
-//     try {
-//         const payload = jwt.verify(token, "mySuperJWTkeySecret940611") as UserPayload;
-//         req.currentUser = payload;
+    try {
+        const payload = jwt.verify(token, "mySuperJWTkeySecret940611") as UserPayload;
+        req.currentUser = payload;
 
-//     } catch (error) {
-//         console.log("JWT verification failed", error);
-//     }
+    } catch (error) {
+        console.log("JWT verification failed", error);
+    }
 
-//     next();
-// }
+    next();
+}
